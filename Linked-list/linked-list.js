5171e0f98ed495f58b5f713903ed3fb94d2a3fc3
Jeffer Silveira <jeffersonsilveira@live.comr>
2024-07-01 21:13:28 -0300

    linked list js update

--- Linked-list/linked-list.js
+++ Linked-list/linked-list.js
@@ -61,19 +61,38 @@ class List{
         }
         return false;
     }
+
+    // Metodo removeAt()
+    removeAt(position){
+        if(position >= 0 && position <= this.length){
+            const current = this.head;
+
+            if(position === 0){
+                this.head = current.next;
+            } else {
+                const previous = this.getElementAt(position -1);
+                const current = previous.next;
+
+                previous.next = current.next;
+            }
+            this.length--;
+            return true;
+        }
+        return false;
+    }
 }
 
 const list = new List();
 
+// console.log(JSON.stringify(list, null, 2));
 list.push(10);
-console.log(list);
 list.push(20);
 list.push(30);
-console.log(list);
-console.log(JSON.stringify(list, null, 2));
 
 // let thirdNode = list.getElementAt(2);
 // console.log(thirdNode);
 
-list.insertAt(25, 2);
+list.insertAt(40, 3);
+
+list.removeAt(3);   // output:   "head": { "value": 10, "next": { "value": 20, "next": { "value": 30 }
 console.log(JSON.stringify(list, null, 2));
