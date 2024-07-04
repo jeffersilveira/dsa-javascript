ce9495b69aaa9801107d2558ba474fc17bddfffe
Jeffer Silveira <jeffersonsilveira@live.comr>
2024-07-04 19:46:02 -0300

    stack js update

--- Stack/stack.js
+++ Stack/stack.js
@@ -5,31 +5,31 @@ class Stack {
         this.items = [];        // array a ser passado aos objetos
     }
 
-    // push(elementos) Incluir elementos no stack
-    push(elements) {
-        this.items.push(elements);
+    // Incluir elementos no stack
+    push(element) {
+        this.items.push(element);
     }
-    // pop() Excluir elementos no stack
+    // Excluir elementos no stack
     pop() {
         return this.items.pop();
     }
 
-    // peek() Verificar elementos no topo da stack
+    // Verificar elementos no topo da stack
     peek() {
         return this.items[this.items.length -1];
     }
     
-    // isEmpty() Verificar se a stack está vazia
+    // Verificar se a stack está vazia
     isEmpty() {
         return this.items.length === 0;     // retorna true our false
     }
 
-    // clear() Limpar a stack
+    // Limpar a stack
     clear() {
         this.items = [];
     }
 
-    // size() Listar elementos incluídos na stack
+    // Listar elementos incluídos na stack
     size() {
         return this.items.length;
     }
@@ -40,22 +40,22 @@ let stack = new Stack();
 stack.push(1);
 stack.push(2);
 console.log(stack);
-stack.clear();      // Limpa os elementos da Stack
+stack.clear();
 console.log(stack);
 
-stack.push(10);     //
-stack.push(20);     //  Inserido elementos na Stack
-stack.push(30);     //
+stack.push(10);
+stack.push(20);
+stack.push(30);
 console.log(stack);
 
-console.log(stack.peek());      // Retorna o ultimo elemento da Stack
+console.log(stack.peek());
 
 stack.pop();
-console.log(stack);     //  Excluido valor '30' e retornado no log
+console.log(stack);
 
-console.log(stack.size());      //  Retorna numero de elementos na Stack
+console.log(stack.size());
 
-console.log(stack.isEmpty());        //  Retorna um boolean se a Stack está vazia
+console.log(stack.isEmpty());
 
 /* OUTPUT:
 Stack { items: [ 1, 2 ] }
